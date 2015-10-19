d1cbd2759af6244c5141b07fd9846a1501bbbf1f
Michael Hoy <mjh@mjhoy.com>
2015-10-19 14:31:33 -0500

    more test setup; project model update

--- db/001-projects.sql
+++ db/001-projects.sql
@@ -1,6 +1,12 @@
 create table projects
 (
+  id serial     primary key,
+
   title         text not null,
   description   text not null,
-  id serial     primary key
+
+  published     boolean not null default false,
+
+  created       timestamp without time zone not null default (now() at time zone 'utc'),
+  updated       timestamp without time zone not null default (now() at time zone 'utc')
 );
